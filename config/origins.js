4138749db069158e044b465d2e8b88f3cd8acc5e
mkhotamirais <mkhotamirais@gmail.com>
2025-02-04 18:43:04 +0700

    origin baru

--- config/origins.js
+++ config/origins.js
@@ -1,1 +1,5 @@
-export const allowedOrigins = ["http://localhost:5173", "http://localhost:5174", "https://vite-api-nu.vercel.app"];
+export const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5174",
+  "https://mkhotami-react-api.vercel.app",
+];
