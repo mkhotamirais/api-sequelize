4b0f8fa284750dc638c1cdbcb7ab80b420a22855
mkhotamirais <mkhotamirais@gmail.com>
2025-03-19 14:53:14 +0700

    production

--- config/index.js
+++ config/index.js
@@ -2,10 +2,10 @@ import { Sequelize } from "sequelize";
 import "dotenv/config";
 import mysql2 from "mysql2";
 
-const db_host = process.env.DB_HOST;
-const db_user = process.env.DB_USER;
-const db_pass = process.env.DB_PASS;
-const db_name = process.env.DB_NAME;
+const db_host = process.env.NODE_ENV === "production" ? process.env.DB_HOST_PROD : process.env.DB_HOST;
+const db_user = process.env.NODE_ENV === "production" ? process.env.DB_USER_PROD : process.env.DB_USER;
+const db_pass = process.env.NODE_ENV === "production" ? process.env.DB_PASS_PROD : process.env.DB_PASS;
+const db_name = process.env.NODE_ENV === "production" ? process.env.DB_NAME_PROD : process.env.DB_NAME;
 // const db_uri = process.env.DB_URI;
 
 const db = new Sequelize(db_name, db_user, db_pass, {
